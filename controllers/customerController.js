759312bafe07111bb454a24fc7ddecbf0b1fd4dd
raflirach <rafli060395@gmail.com>
2020-12-31 06:16:23 +0700

    done release 6

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -10,6 +10,23 @@ class Controller {
        }) 
        .catch(e => res.send(e))
     }
+
+    static showFormRegister(req,res){
+        res.render('formRegister')
+    }
+
+    static register(req,res){
+        const input = {
+            identityNumber: req.body.identityNumber ? req.body.identityNumber : null,
+            fullName: req.body.fullName ? req.body.fullName : null,
+            address: req.body.address ? req.body.address : null,
+            birthDate: req.body.birthDate ? req.body.birthDate : null,
+            gender: req.body.gender ? req.body.gender : null,
+        }
+        Customer.create(input)
+        .then( _=> res.redirect('/customers'))
+        .catch(e => res.send(e))
+    }
 }
 
 module.exports = Controller
--- routes/customers.js
+++ routes/customers.js
@@ -4,5 +4,9 @@ const router = require('express').Router()
 
 router.get('/', Controller.showList)
 
+router.route('/register')
+    .get(Controller.showFormRegister)
+    .post(Controller.register)
+
 
 module.exports = router
